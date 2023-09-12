93e72ecd151eb6ddbd406096feaafec5af21a48a
Michaela <botka@email.cz>
2023-09-12 10:07:30 +0200

    Expandující fotogalerie

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const slides = document.querySelectorAll(".slide");
 
-slides.forEach(function (oneslide){
-    oneslide.addEventListener("click",function(){
+slides.forEach(function (oneslide) {
+    oneslide.addEventListener("click", function () {
         smazaniActive();
         oneslide.classList.add("active");
     })
-
 });
 
-function smazaniActive(){
-    slides.forEach(function(mySlide) {
+function smazaniActive() {
+    slides.forEach(function (mySlide) {
         mySlide.classList.remove("active");
     })
 };
